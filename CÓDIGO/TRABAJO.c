91a98ddb4098ef9b6475cf63ec2c88382355e378
AlvaroMartin3 <a.mcastillo@alumnos.upm.es>
2023-05-26 14:01:10 +0200

    Cambio manera de tratar datos fichero
    Hemos cambiado la forma de tratar los datos del fichero y pasado a float los datos del mismo, para poder trabajar con ellos

--- CÓDIGO/TRABAJO.c
+++ CÓDIGO/TRABAJO.c
@@ -1,123 +1,172 @@
 #include <stdio.h>
-#include <locale.h>
 #include <string.h>
-#include <string.h>
+#include <stdlib.h>
 
-struct cadenas{
-	char filasdedatos[1000];
-};
+int creaTabla(char *filas[])
+{
+	printf("Crear Tabla\n");
+	return(0);
+}
+
+float media (float matriz[][24], int n);
 
 int main()
 {
-    setlocale(LC_CTYPE,"spanish");
 
-    FILE *datos;
-    struct cadenas *filas;
-    char c;
+	FILE *datos;
+	char matrizdatos[25][30][256];
+	int elegir, n = 0, i = 0,j, numcomas = 0, maxcomas = 0;
+	char c;
+	float matriz[500][500];
+	float valormedia;
 
-    int elegir, n, i;
+				//datos = fopen("genera.txt", "r");
+    fopen_s(& datos, "generacion_por_tecnologias_21_22_puntos_simplificado.csv", "r");
 
-    printf("    DATOS DE GENERACIÓN ELÉCTRICA    \n");
-    printf("*************************************\n\n");
-    printf("En el menú se muestran diferenciados por números las distintas estadísticas energéticas\n\n");
-
-    //imprimimos menu para que elija el usuario
-    printf("1.-Cargar y mostrar datos del archivo:\n");
-    printf("2.-Estadistica de la energia que mayor consumo presenta\n");
-    printf("3.-Seleccionar energia y mostrar su consumo por meses\n");
-    printf("4.-Calcular la media de consumo de una energía por meses\n");
-    printf("5.-Seleccionar una energia y mandarla a otro fichero\n");
-    printf("6.-¿En qué mes se produce mas energía entre todos los tipos de generación?\n");
-    printf("7.-Ordenar consumo de mayor a menor de cada energía\n");
-    printf("8.-FIN\n");
-
-    do
+    if (datos == NULL)
     {
-    printf("Introduzca un valor\n");
-    scanf("%d", &elegir);
-    switch (elegir)
+        printf("Fichero no abierto\n");
+
+    }else
     {
-    case 1:
+        printf("Abierto correctamente\n\n");
+    }
+
+    i = 0;
+    n = 0;
+    numcomas = 0;
+    maxcomas = 0;
+    for (int i = 0; i < 25; i++)
+    {
+        for (int j = 0; j < 30; j++)
         {
-            printf("Cargar y mostrar datos del archivo:\n");
-
-            datos = fopen("generacion_por_tecnologias_21_22_puntos_simplificado.csv","r");
-
-            if (datos == NULL)
+            for (int k = 0; k < 256; k++)
             {
-                printf("Fichero no abierto\n");
-            }else{
-                printf("Abierto correctamente\n\n");
+                matrizdatos[i][j][k] = 0;
             }
-
-            while(!feof(datos))
-            {
-	                c=fgetc(datos);
-	                if (c=='\n') n++;
-            }
-            rewind(datos);
-
-            filas = (struct cadenas*)malloc(n*sizeof(struct cadenas));
-
-            for(i=0;i<n;i++)
-            {
-	            fgets(filas[i].filasdedatos,sizeof(filas[i].filasdedatos),datos);
-	            filas[i].filasdedatos[strlen(filas[i].filasdedatos)-1]='\0';
-            }
-
-
-            for(i=0;i<n;i++)//IMPRIMIMOS DATOS DEL FICHERO
-            {
-                printf ("%s\n",filas[i].filasdedatos);
-
-            }
-
-
-            fclose(datos);
-
-            break;
-        }
-    case 2:
-        {
-            printf("Estadistica de la energia que mayor consumo presenta\n");
-            break;
-        }
-    case 3:
-        {
-            printf("Seleccionar energia y mostrar su consumo por meses\n");
-            break;
-        }
-    case 4:
-        {
-            printf("Calcular la media de consumo de una energía por meses\n");
-            break;
-        }
-    case 5:
-        {
-            printf("Seleccionar una energia y mandarla a otro fichero\n");
-            break;
-        }
-    case 6:
-        {
-            printf("¿En qué mes se produce mas energía entre todos los tipos de generación?\n");
-            break;
-        }
-    case 7:
-        {
-            printf("Ordenar consumo de mayor a menor de cada energía\n");
-            break;
-        }
-    case 8:
-        {
-            printf("FIN\n");
-            return 0;
-        }
-    default:
-        {
-            printf("ERROR\n");
         }
     }
-    }while (elegir != 8);
 
-return 0;
+    while (!feof(datos))
+    {
+        c = fgetc(datos);
+
+        if (c == ',')
+        {
+            matrizdatos[n][numcomas][i] = 0;
+            numcomas++;
+            if (numcomas > maxcomas) maxcomas = numcomas;
+                i = 0;
+                continue;
+        }else if (c == '\n')
+        {
+            matrizdatos[n][numcomas][i] = 0;
+            numcomas++;
+            if (numcomas > maxcomas) maxcomas = numcomas;
+					n++;
+					numcomas = 0;
+					i = 0;
+					//if (n > 4) break;
+        }else
+        {
+            matrizdatos[n][numcomas][i++] = c;
+        }
+
+    }
+fclose(datos);
+
+
+
+	    for (int i = 5; i < n; i++) {
+        for (int j = 0; j < maxcomas; j++) {
+
+                matriz[i-5][j-1] = atof(matrizdatos[i][j]);
+        }
+    }
+
+	do
+	{
+		//imprimimos menu para que elija el usuario
+		printf("*************************************\n\n");
+		printf("*    DATOS DE GENERACIÓN ELÉCTRICA  *\n");
+		printf("*************************************\n\n");
+		printf("En el menú se muestran diferenciados por números las distintas estadísticas energéticas\n\n");
+	    printf("1.-Cargar y mostrar datos del archivo:\n");
+		printf("2.-Estadistica de la energia que mayor consumo presenta\n");
+		printf("3.-Seleccionar energia y mostrar su consumo por meses\n");
+		printf("4.-Calcular la media de consumo de una energía por meses\n");
+		printf("5.-Seleccionar una energia y mandarla a otro fichero\n");
+		printf("6.-¿En qué mes se produce mas energía entre todos los tipos de generación?\n");
+		printf("7.-Ordenar consumo de mayor a menor de cada energía\n");
+		printf("8.-FIN\n");
+
+		printf("Introduzca un valor\n");
+		//scanf("%d", &elegir);
+		scanf_s("%d", &elegir);
+
+
+		switch (elegir)
+		{
+		case 1:
+			printf("Cargar y mostrar datos del archivo:\n");
+			//datos = fopen("genera.txt", "r");
+
+			printf("Numero de lineas: %d\n", n);
+			printf("Numero de columnas: %d\n", maxcomas);
+
+			for (int i = 0; i < n; i++)
+			{
+				printf("LINEA[%d]:\n", i);
+				for (int j = 0; j < maxcomas; j++)
+				{
+					printf("-->%s<--\n",matrizdatos[i][j]);
+				}
+				printf("-------------- FIN LINEA(%d) ----------------------\n", i);
+			}
+			break;
+		case 2:
+		{
+			printf("Estadistica de la energia que mayor consumo presenta\n");
+
+			break;
+		}
+		case 3:
+		{
+			printf("Seleccionar energia y mostrar su consumo por meses\n");
+			break;
+		}
+		case 4:
+		{
+			printf("Calcular la media de consumo de una energía por meses\n");
+
+			break;
+		}
+		case 5:
+		{
+			printf("Seleccionar una energia y mandarla a otro fichero\n");
+			break;
+		}
+		case 6:
+		{
+			printf("¿En qué mes se produce mas energía entre todos los tipos de generación?\n");
+			break;
+		}
+		case 7:
+		{
+			printf("Ordenar consumo de mayor a menor de cada energía\n");
+			break;
+		}
+		case 8:
+		{
+			printf("FIN\n");
+			return 0;
+		}
+		default:
+		{
+			printf("ERROR\n");
+		}
+		}
+	} while (elegir != 8);
+
+	return 0;
 }
